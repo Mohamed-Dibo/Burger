b7b8718daed6fbe811b7342fde139080c62b2704
mohamed diab <diaboom650@gmail.com>
2021-06-21 16:49:48 +0200

    updated 5

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/Burger/'>
       <App />
     </BrowserRouter>
   </Provider>
